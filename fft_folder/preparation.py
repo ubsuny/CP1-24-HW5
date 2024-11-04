7adc499108fccb3c3e193ccede53ef8873c9f1f8
zbpetersbuf <zbpeters@buffalo.edu>
2024-11-04 18:34:08 +0000

    newist i hope this is it

--- fft_folder/preparation.py
+++ fft_folder/preparation.py
@@ -6,8 +6,29 @@ inverse fft
 calculate frequencies
 """
 import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
 
 def fft_powerspectrum(data):
-    """This function interprates the csv data for you and calc the fft"""
-    matrx = np.fft.fft(data)
+    """This function takes the function in and outputs
+    the powerspectrum"""
+    data1 = np.zeros(len(data))
+    i=0
+    while i < len(data):
+        data1[i] = data.iloc[i]
+        i+=1
+    matrx = np.fft.fft(data1)
     return np.abs(matrx)[:len(matrx/2)]
+
+def fft_mag(data):
+    """this function is simalare to fft_powerspectrum only it does not cut the
+    matrix in half or take the absolut values of the variables"""
+    data1 = np.zeros(len(data))
+    i=0
+    while i < len(data):
+        data1[i] = data.iloc[i]
+        i+=1
+    return np.fft.fft(data1)
+
+
+
