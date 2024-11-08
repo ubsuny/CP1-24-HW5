a92357c4689e1886dda5c73646d12e28a44b641c
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-11-08 09:43:09 -0500

    Update preparation.py
    fixed pylint error.

--- preparation.py
+++ preparation.py
@@ -1,8 +1,9 @@
 
-
+"""
 This module does padding and unpadding the data
 """
 import pandas as pd
+import numpy as np
 
 def pad_time_series(series, target_length, padding_value=None):
     """
@@ -60,96 +61,3 @@ def unpad_time_series(padded_series, original_length):
     """
     # Return the original length of the series
     return padded_series.iloc[:original_length]
-  
-fft, invs ffft and to calculate the frequencies
-fft
-inverse fft
-calculate frequencies
-"""
-
-import numpy as np
-import pandas as pd
-
-
-def fft_powerspectrum(data):
-    """This function takes the function in and outputs
-    the powerspectrum"""
-    n = len(data)
-    timestamp_sum = sum(data.index[i+1].timestamp() - data.index[i].timestamp() for i in range(n-1))
-    if not timestamp_sum/(n-1) == data.index[2].timestamp() - data.index[1].timestamp():
-        print("Data is not evenly spaced or data points are missing")
-        return None
-    matrx = np.fft.fft(data.values)
-    return np.abs(matrx)[:n // 2]
-
-
-def fft_mag(data):
-    """this function is simalare to fft_powerspectrum only it does not cut the
-    matrix in half or take the absolut values of the variables"""
-    n = len(data)
-    timestamp_sum = sum(data.index[i+1].timestamp() - data.index[i].timestamp() for i in range(n-1))
-    if not timestamp_sum/(n-1) == data.index[2].timestamp() - data.index[1].timestamp():
-        print("Data is not evenly spaced or data points are missing")
-        return None
-    return np.fft.fft(data.values)
-
-def inv_fft(mag):
-    """this invers fft's input is the output of the fft_mag function
-    do not enter in the fft_powerspectrum cause the output of this would be wrong
-    ie not inv_fft(fft_powerspectrum(data)), but inv_fft(fft_mag(data))"""
-    newthing = np.fft.ifft(mag)
-    return np.abs(newthing)
-
-def calc_freq(data, tim):
-    """this takes in the same data as the fft equations only gives the frequencies
-    of the data, this gives out the frequencies in Hz, if you want to change it to
-    days say day in the second imput, or if you want it in months say month (ie 365.25/12 days) """
-    n = len(data)
-    timestamp_sum = sum(data.index[i+1].timestamp() - data.index[i].timestamp() for i in range(n-1))
-    diftim = data.index[2].timestamp() - data.index[1].timestamp()
-    if not timestamp_sum/(n-1) == diftim:
-        print("Data is not evenly spaced or data points are missing")
-        return None
-    if tim.strip().lower() == 'day':
-        diftim = diftim/(60*60*24)
-    if tim.strip().lower() == 'month':
-        diftim = diftim/(60*60*24*30.4375)
-    return np.fft.fftfreq(n, d = diftim)
-
-def get_timeseries(path):
-    '''
-    This function reads json files from the data collection task 
-    and returns a pandas time series with datetime as index and 
-    concentration of CO2/Methane as data.
-
-    Once the path is specified, we can plot the time series using a simple matplotlib code
-    plt.figure(figsize=(10, 6))
-    co2_series.plot()
-    plt.title("CO2 Concentration Over Time")
-    plt.xlabel("Date")
-    plt.ylabel("CO2 (ppm)")
-    plt.grid(True)
-    plt.show()
-
-    Parameters:
-    - path: Stringlike. path/to/json/file.json
-
-    Returns:
-    - Pandas Time Series. Index = Datetime, Data = CO2/Methane Concentration
-    '''
-
-    #Extracts data from the json file at the input path
-    data = pd.read_json(path)
-    #Uses the month and year information from the json file,
-    # assumes data was taken on the first of each month,
-    # creates new column with datetime
-    data['date'] = pd.to_datetime(data[['Year', 'Month']].assign(Day=1))
-
-    #Sets datetime as index
-    data.set_index('date', inplace=True)
-
-    #Creates timeseries with co2 (ppm) as data and datetime as index
-    co2_series = data['CO2 (ppm)']
-
-
-    return co2_series
