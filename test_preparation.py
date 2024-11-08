3b253e53187b04961e0c0363ee76dbaa29f28ac9
abruns123 <aabruns123@gmail.com>
2024-11-07 21:41:51 -0500

    Update test_preparation.py

--- test_preparation.py
+++ test_preparation.py
@@ -43,20 +43,26 @@ def data():
     return pd.Series([1, 2, 3, 2, 1, 2, 3, 2, 1, 2], index=trange)
 
 def test_fft_powerspectrum(data):
-    """test the powerspectrums length and type of export"""
+    """
+    test the powerspectrums length and type of export
+    """
     powrspec = fft_powerspectrum(data)
     assert len(powrspec) == len(data)/2
     assert isinstance(powrspec, np.ndarray)
 
 def test_fft_mag(data):
-    """test the fft_mag length and type of export"""
+    """
+    test the fft_mag length and type of export
+    """
     magnitudes = fft_mag(data)
     assert len(magnitudes) == len(data)
     assert isinstance(magnitudes, np.ndarray)
 
 def test_inv_fft(data):
-    """tests inv_fft length export aswell as testing to see if
-    the fit matches the data"""
+    """
+    tests inv_fft length export aswell as testing to see if
+    the fit matches the data
+    """
     invdata = inv_fft(fft_mag(data))
     assert len(invdata) == len(data)
     assert np.allclose(invdata, data.values, atol=1e-4)
