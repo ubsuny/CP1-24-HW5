793babcdef31f33dedf5eaf0d08f3cc16e1e7d42
abruns123 <aabruns123@gmail.com>
2024-11-08 16:47:32 +0000

    fixed quotation mark pylint errors

--- test_preparation.py
+++ test_preparation.py
@@ -44,32 +44,24 @@ def data():
     return pd.Series([1, 2, 3, 2, 1, 2, 3, 2, 1, 2], index=trange)
     """
 def test_fft_powerspectrum(data):
-    """
-    """
+
     test the powerspectrums length and type of export
-    """
-    """
     powrspec = fft_powerspectrum(data)
     assert len(powrspec) == len(data)/2
     assert isinstance(powrspec, np.ndarray)
 
 def test_fft_mag(data):
-    """
-    """
-    test the fft_mag length and type of export
-    """
-    """
+
+    test the fft_mag length and type of expor
     magnitudes = fft_mag(data)
     assert len(magnitudes) == len(data)
     assert isinstance(magnitudes, np.ndarray)
 
 def test_inv_fft(data):
-    """
-    """
+
     tests inv_fft length export aswell as testing to see if
     the fit matches the data
-    """
-    """
+
     invdata = inv_fft(fft_mag(data))
     assert len(invdata) == len(data)
     assert np.allclose(invdata, data.values, atol=1e-4)
@@ -78,14 +70,19 @@ def test_inv_fft(data):
 trange = pd.date_range(datetime.now(), datetime.now()+timedelta(days=9),freq='d')
 data1 = pd.Series([1,2,3,4,5,6,7,8,9,10],index=trange)
 
-"""
-"""
 print(len(data1))
 
 print(data1.index[0].timestamp())
 
 
 print(data1.iloc[2] * 1)
+
+def test_calc_freq(data):
+
+    calcs the length of the export of calc_freq
+
+    freq = calc_freq(data, 'seconds')
+    assert len(freq) == len(data)
 """
 
 
@@ -147,13 +144,5 @@ def test_unwindow():
     for i in test_func:
         assert round(i,3)==round(new.values[count],3)
         count+=1
-"""
-def test_calc_freq(data):
-"""
-"""
-    calcs the length of the export of calc_freq
-    """
-"""
-    freq = calc_freq(data, 'seconds')
-    assert len(freq) == len(data)
-    """
+
+
